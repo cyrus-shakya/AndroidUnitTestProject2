edfc0f2da8c457394992b771a5b9bc5f2b1fa9ba
cyrus-shakya <shakya.cyrus@gmail.com>
2023-11-29 19:38:33 -0500

    initial commit

---
+++ LocalTesting 2/app/src/main/java/jlin2/examples/localtesting/MainActivity.kt
@@ -0,0 +1,11 @@
+package jlin2.examples.localtesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
